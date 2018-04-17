913058602d7fd93f13e3d02cd1899514c3e88971
user <regwebghost@yandex.ru>
2018-04-17 13:33:07 +0700

    add some basic functions

--- main-project/ssh-transport.py
+++ main-project/ssh-transport.py
@@ -6,14 +6,31 @@ def get_transport(transport_name, host, port, login, password):
     pass
 
 class SSHtransport():
-    def __init__(self):
-        pass
+    # Connection default values
+    def_host = 'localhost'
+    def_port = 22022
+    def_login = 'root'
+    def_password = 'pwd'
+
+    def __init__(self, host = def_host, port = def_port, login = def_login, password = def_password):
+        self.client = paramiko.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+        self.client.connect(hostname = host, username = login, password = password, port = port)
 
     def __del__(self):
-        pass
+        self.client.close()
 
-    def exec(self, command):
-        pass
+    def exec(self, command = 'uname -a'):
+        stdin, stdout, stderr = self.client.exec_command(command)
+        return stdout.read()
 
     def get_file(self, path):
+        # work on it
         pass
+
+def main():
+    base_client = SSHtransport('127.0.0.1', 22022, 'root', 'pwd')
+    print(base_client.exec())
+
+if __name__ == "__main__":
+    main()
