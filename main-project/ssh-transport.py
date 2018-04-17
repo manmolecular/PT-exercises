3ce75eb683c94143f2fa338ae1eb651ae0419d44
user <regwebghost@yandex.ru>
2018-04-18 00:32:37 +0700

    refactoring

--- main-project/ssh-transport.py
+++ main-project/ssh-transport.py
@@ -2,47 +2,54 @@
 # SSH transport class based on PT-security lectures
 import paramiko
 
+default_host = 'localhost'
+default_port = 22022
+default_login = 'root'
+default_password = 'pwd'
+
+# Class for error handling
 class TransportError(Exception):
     def __init__(self, message = 'unknown error'):
         self.full_message = 'TransportError: ' + message
         print(self.full_message)
 
-def get_transport(transport_name, host, port, login, password):
-    pass    
-
+# SSH transport class
 class SSHtransport():
-    def __init__(self, host = 'localhost', port = 22022, login = 'root', password = 'pwd'):
+    def __init__(self, host = default_host, port = default_port, login = default_login, password = default_password):
         try:
             self.client = paramiko.SSHClient()
             self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
             self.client.connect(hostname = host, username = login, password = password, port = port)
         except:
-            TransportError("can't connect to host")
+            TransportError('can not connect to host')
             
     def __del__(self):
         self.client.close()
 
     def exec(self, command = ''):
-        if (command == ''):
-            raise TransportError('command is empty')
+        if not command:
+            raise TransportError('argument *command* is empty')
         else:
             stdin, stdout, stderr = self.client.exec_command(command)
             return stdout.read()
 
     def get_file(self, file_name = '', remote_path = './', local_path = './'):
-        if (file_name == ''):
-            raise TransportError('file is empty')
+        if not file_name:
+            raise TransportError('argument *file_name* is empty')
         else:
             file_remote = remote_path + file_name
             file_local = local_path + file_name
             sftp = self.client.open_sftp()
             sftp.get(file_remote, file_local)
             sftp.close()
+
+def get_transport(transport_name, host = default_host, port = default_port, login = default_login, password = default_password):
+    return transport_name(host, port, login, password);
         
 def main():
-    base_client = SSHtransport()
+    base_client = get_transport(SSHtransport)
     print( base_client.exec('ls') )
-    base_client.get_file('')
+    base_client.get_file('getme')
 
 if __name__ == "__main__":
     main()
