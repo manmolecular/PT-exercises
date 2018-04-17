b36cd7c2e2d009a63781a6852aa226cbd5dd12f6
user <regwebghost@yandex.ru>
2018-04-17 13:01:16 +0700

    add basic structure of code

---
+++ main-project/ssh-transport.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# SSH transport class based on PT-security lectures
+import paramiko
+
+def get_transport(transport_name, host, port, login, password):
+    pass
+
+class SSHtransport():
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+
+    def exec(self, command):
+        pass
+
+    def get_file(self, path):
+        pass
