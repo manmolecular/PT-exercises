9d1940d3512e351388a49c3c1f6266cc70500f3d
user <regwebghost@yandex.ru>
2018-04-18 17:55:42 +0700

    add tests

--- main-project/unit_test.py
+++ main-project/unit_test.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
-from ssh_transport import SSHtransport
+# Tests for SSH transport class based on PT-security lectures
+from ssh_transport import *
 import pytest
 
-def test_transport():
-    # some code for testing
-    pass
+def test_get_transport_exc():
+    with pytest.raises(UnknownTransport):
+        get_transport('', 'localhost', '22022', 'root', 'pwd')
+
+def test_init_exc():
+    with pytest.raises(TransportConnectionError):
+        SSHtransport('localhot', '22022', 'root', 'pwd')
+
+def test_exec_exc():
+    with pytest.raises(TransportError):
+        SSHtransport('localhost', '22022', 'root', 'pwd').exec('')
+
+def test_get_file_name_exc():
+    with pytest.raises(TransportError):
+        SSHtransport('localhost', '22022', 'root', 'pwd').get_file('')
+
+def test_get_file_remote_exc():
+    with pytest.raises(TransportError):
+        SSHtransport('localhost', '22022', 'root', 'pwd').get_file('_unknownfile_')
