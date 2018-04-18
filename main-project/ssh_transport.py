36c28f15876649803b733a5f9213d930b62da46b
user <regwebghost@yandex.ru>
2018-04-18 17:18:18 +0700

    new exception classes

--- main-project/ssh_transport.py
+++ main-project/ssh_transport.py
@@ -1,59 +1,72 @@
 #!/usr/bin/env python3
 # SSH transport class based on PT-security lectures
 import paramiko
+import traceback
 
-default_host = 'localhost'
-default_port = 22022
-default_login = 'root'
-default_password = 'pwd'
+defaults = {'host': 'localhost', 'port':22022, 'login':'root', 'password':'pwd'}
+transport_names = ['SSHtransport']
 
-# Class for error handling
+# Classes for error handling
+class UnknownTransport(Exception):
+    def __init__(self, error_args):
+        print(error_args)
+        Exception.__init__(self, 'UnknownTransport {}'.format(error_args))
+        self.error_args = error_args
+
 class TransportError(Exception):
-    def __init__(self, message = 'unknown error'):
-        self.full_message = 'TransportError: ' + message
-        print(self.full_message)
+    def __init__(self, error_args):
+        print(error_args)
+        Exception.__init__(self, 'TransportError {}'.format(error_args))
+        self.error_args = error_args
+
+class TransportConnectionError(Exception):
+    def __init__(self, error_args):
+        print(error_args)
+        Exception.__init__(self, 'TransportConnectionError {}'.format(error_args))
+        self.error_args = error_args
 
 # SSH transport class
 class SSHtransport():
-    def __init__(self, host = default_host, port = default_port, login = default_login, password = default_password):
+    def __init__(self, host, port, login, password):
+        self.client = paramiko.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
         try:
-            self.client = paramiko.SSHClient()
-            self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
             self.client.connect(hostname = host, username = login, password = password, port = port)
         except:
-            TransportError('can not connect to host')
+            raise TransportConnectionError('{connection refused}')
             
     def __del__(self):
         self.client.close()
 
     def exec(self, command = ''):
         if not command:
-            raise TransportError('argument *command* is empty')
-        else:
-            stdin, stdout, stderr = self.client.exec_command(command)
-            return stdout.read()
+            raise TransportError({'command':command})
+        stdin, stdout, stderr = self.client.exec_command(command)
+        return stdout.read()
 
     def get_file(self, file_name = '', remote_path = './', local_path = './'):
         if not file_name:
-            raise TransportError('argument *file_name* is empty')
-        else:
-            file_remote = remote_path + file_name
-            file_local = local_path + file_name
-            sftp = self.client.open_sftp()
-            sftp.get(file_remote, file_local)
-            sftp.close()
+            raise TransportError({'file_name':file_name})
+        file_remote = remote_path + file_name
+        file_local = local_path + file_name
+        sftp = self.client.open_sftp()
+        try:
+            sftp.stat(file_remote)
+        except:
+            raise TransportError('{file doesnt exist}')
+        sftp.get(file_remote, file_local)
+        sftp.close()
 
 # Get unique transport of some class
-def get_transport(transport_name, host = default_host, port = default_port, login = default_login, password = default_password):
-    try:
-        return transport_name(host, port, login, password);
-    except:
-        TransportError('Unknown transport')
+def get_transport(transport_name, host, port, login, password):
+    if transport_name not in transport_names:
+        raise UnknownTransport({'transport_name':transport_name})
+    return globals()[transport_name](host, port, login, password)
 
 def main():
-    base_client = get_transport(SSHtransport)
-    print( base_client.exec('ls') )
-    base_client.get_file('getme')
+    base_client = get_transport('SSHtransport', 'localhost', '22022', 'root', 'pwd')
+    print( base_client.exec('ls -a') )
+    base_client.get_file('get2me')
 
 if __name__ == "__main__":
     main()
