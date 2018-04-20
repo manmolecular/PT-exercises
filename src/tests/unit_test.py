a091652f3c02463b58a8ffee6f9e65a608c8c717
user <regwebghost@yandex.ru>
2018-04-21 00:12:28 +0700

    new tests

--- src/tests/unit_test.py
+++ src/tests/unit_test.py
@@ -4,18 +4,22 @@ from ssh_transport import *
 import pytest
 
 def test_get_transport_exc():
+    get_transport('SSHtransport', 'localhost', '22022', 'root', 'pwd')
     with pytest.raises(UnknownTransport):
         get_transport('', 'localhost', '22022', 'root', 'pwd')
 
 def test_init_exc():
+    SSHtransport('localhost', '22022', 'root', 'pwd')
     with pytest.raises(TransportConnectionError):
         SSHtransport('localhot', '22022', 'root', 'pwd')
 
 def test_exec_exc():
+    SSHtransport('localhost', '22022', 'root', 'pwd').exec('ls')
     with pytest.raises(TransportError):
         SSHtransport('localhost', '22022', 'root', 'pwd').exec('')
 
 def test_get_file_name_exc():
+    SSHtransport('localhost', '22022', 'root', 'pwd').get_file('getme')
     with pytest.raises(TransportError):
         SSHtransport('localhost', '22022', 'root', 'pwd').get_file('')
 
