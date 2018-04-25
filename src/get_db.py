8bd66ccc7e17657f258d91ab9c3d2883aa9ed601
user <regwebghost@yandex.ru>
2018-04-26 01:13:55 +0700

    add database module

---
+++ src/get_db.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# Create database
+import json
+import sqlite3
+import os.path
+
+_json_db = None
+_db_name = 'controls.json'
+
+def get_full_path():
+    my_path = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(my_path, _db_name)
+
+def get_db():
+    global _json_db
+    if not _json_db:
+        with open(get_full_path(),'r') as f:
+            _json_db = json.load(f)
+    return _json_db
+
+def create_db():
+    db = sqlite3.connect('database.db')
+    curr = db.cursor()
+    curr.execute('''CREATE TABLE if not exists
+        control(id INTEGER PRIMARY KEY, descr TEXT)''')
+
+    controls = get_db()
+    for string in controls:
+        curr.execute("INSERT INTO control(id, descr) VALUES(?, ?)", (string[0], string[1]))
+    db.commit()
+    db.close()
+
+create_db()
